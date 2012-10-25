92c252071de7341b60fa97ee0927291f7ac157f1
EC2 Default User <ec2-user@ip-10-132-79-176.ap-northeast-1.compute.internal>
2012-10-25 15:59:27 +0000

    convert to using thread

--- linux_write_test/log_test.py
+++ linux_write_test/log_test.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import logging
+logname='/ebs/log/myapp.log'
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    filename=logname,
+    filemode='a')
 import time
 import os
-logname='/ebs/log/myapp.log'
-log_file=open(logname,'ab')
-mx=300000000
-count=0
-div=100000
-prevtime=time.time()
-prevcnt=count
-while count<mx:
-    count+=1
-    msg='cnt=%010d\n' % count
-    log_file.write(msg)
-    log_file.flush()
-    if count%div==0:
-        tm=time.time()-prevtime
-        diffcnt=count-prevcnt
-        fsize=os.path.getsize(logname)
-        fsize=fsize/(1024*1024)
-        msg='count=%010d,diffcnt=%d,fsize=%d MB,time=%f\n' % (count,diffcnt,fsize,tm)
-        log_file.write(msg)
-        log_file.flush()
-        print msg
-        prevcnt=count
-        prevtime=time.time()
+import threading
+mx=30000000
+div=10000
+def worker():
+    count=0
+    prevtime=time.time()
+    prevcnt=count
+    while count<mx:
+        crr= threading.currentThread().getName()
+        count+=1
+        msg='crrthread=%s,cnt=%010d' % (crr,count)
+        logging.info(msg)
+        if count%div==0:
+            tm=time.time()-prevtime
+            diffcnt=count-prevcnt
+            fsize=os.path.getsize(logname)
+            fsize=fsize/(1024*1024)
+            msg='crthreading=%s,count=%010d,diffcnt=%d,fsize=%d MB,time=%f' % \
+                (crr,count,diffcnt,fsize,tm)
+            print msg
+            logging.info(msg)
+            prevcnt=count
+            prevtime=time.time()
 
-
-
+threads=[]
+for _ in range(10):
+    t=threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
